1197fa917bd45a50ca336149dba509b8357bbe89
FUGERAY <hugo.fugeray@gmail.com>
2023-05-10 01:58:42 +0200

    h

--- test_integration.py
+++ test_integration.py
@@ -3,7 +3,7 @@ import httpx
 from api import api
 
 def test_call_external_api():
-    endpoint = "/cutomers/"  # Utilisez un endpoint valide pour les tests
+    endpoint = "/customers"  # Utilisez un endpoint valide pour les tests
     response = api(endpoint)
 
     assert response.status_code == 200
