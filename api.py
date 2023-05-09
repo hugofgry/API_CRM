fbe85746f98db9e3d7340465402d93f0ec14472c
FUGERAY <hugo.fugeray@gmail.com>
2023-05-09 17:54:34 +0200

    m

--- api.py
+++ api.py
@@ -39,6 +39,4 @@ async def protected_data_products(customer_id: str, order_id: str, token_data: T
     except HTTPException as e:
         return {"data": "custumer_id or order_id do not exists !"}
 
-@app.get("/fgg")
-async def protected_data_products():
-  print('hello')
+
