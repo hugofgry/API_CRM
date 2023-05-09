e1f3f4038ab94b09ab1ac6358ea6e961f8150de8
FUGERAY <hugo.fugeray@gmail.com>
2023-05-10 01:49:46 +0200

    h

---
+++ test_integration.py
@@ -0,0 +1,10 @@
+import pytest
+import httpx
+from api import call_external_api
+
+def test_call_external_api():
+    endpoint = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/"  # Utilisez un endpoint valide pour les tests
+    response = call_external_api(endpoint)
+
+    assert response.status_code == 200
+    assert "data" in response.json()
