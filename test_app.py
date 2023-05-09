01244d55a9bd2545514d9f5bf573fe13726e01ca
FUGERAY <hugo.fugeray@gmail.com>
2023-05-09 20:43:31 +0200

    test

--- test_app.py
+++ test_app.py
@@ -1,21 +1,24 @@
 import pytest
 import httpx
-import os
+import response_orders
 
-token = os.environ['TOKEN']
+token = os.env
+
+for key, value in os.environ.items():
+    print(f"{key}: {value}")
 
 API_BASE_URL = "http://127.0.0.1:8080"
 
 def test_customers():
 
-    headers = {"Authorization": f"Bearer {token}"}
+    headers = {"Authorization": f"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ3ZWJzaG9wIn0.GNG37k32Tc33t1sR8klvnC5qSQNYc80g7jtbswhjsek"}
     response = httpx.get(f"{API_BASE_URL}/customers", headers=headers)
     assert response.status_code == 200
 
 def test_orders():
 
     id = 8  # Un exemple d'ID valide
-    headers = {"Authorization": f"Bearer {token}"}
+    headers = {"Authorization": f"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ3ZWJzaG9wIn0.GNG37k32Tc33t1sR8klvnC5qSQNYc80g7jtbswhjsek"}
     response = httpx.get(f"{API_BASE_URL}/orders?id={id}", headers=headers)
     assert response.status_code == 200
 
@@ -23,7 +26,7 @@ def test_products():
 
     customer_id = 8  # Un exemple d'ID de client valide
     order_id = 8  # Un exemple d'ID de commande valide
-    headers = {"Authorization": f"Bearer {token}"}
+    headers = {"Authorization": f"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ3ZWJzaG9wMiJ9.ppQNBvnNXB8eT_J_bD32fx8Bgimw0KjUaBldDD2lGYQ"}
     response = httpx.get(
         f"{API_BASE_URL}/products?customer_id={customer_id}&order_id={order_id}",
         headers=headers,
@@ -33,7 +36,7 @@ def test_products():
 def test_orders_invalid_customer_id():
 
     invalid_id = 9999  # Un exemple d'ID client invalide
-    headers = {"Authorization": f"Bearer {token}"}
+    headers = {"Authorization": f"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ3ZWJzaG9wMiJ9.ppQNBvnNXB8eT_J_bD32fx8Bgimw0KjUaBldDD2lGYQ"}
     response = httpx.get(f"{API_BASE_URL}/orders?id={invalid_id}", headers=headers)
     assert response.status_code == 200
     assert response.json()["data"] == "custumer_id do not exists !"
@@ -42,7 +45,7 @@ def test_products_invalid_ids():
 
     invalid_customer_id = 9999  # Un exemple d'ID client invalide
     invalid_order_id = 9999  # Un exemple d'ID commande invalide
-    headers = {"Authorization": f"Bearer {token}"}
+    headers = {"Authorization": f"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ3ZWJzaG9wMiJ9.ppQNBvnNXB8eT_J_bD32fx8Bgimw0KjUaBldDD2lGYQ"}
     response = httpx.get(
         f"{API_BASE_URL}/products?customer_id={invalid_customer_id}&order_id={invalid_order_id}",
         headers=headers,
