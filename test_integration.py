6d79069a16c0480f9c456b3056d9286189736ecd
FUGERAY <hugo.fugeray@gmail.com>
2023-05-10 01:55:04 +0200

    h

--- test_integration.py
+++ test_integration.py
@@ -3,7 +3,7 @@ import httpx
 from api import api
 
 def test_call_external_api():
-    endpoint = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/cutomers"  # Utilisez un endpoint valide pour les tests
+    endpoint = "/cutomers"  # Utilisez un endpoint valide pour les tests
     response = api(endpoint)
 
     assert response.status_code == 200
