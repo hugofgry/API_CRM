ac4d9c58bc7ca3031cde7bbb49c8477eadcb87fc
FUGERAY <hugo.fugeray@gmail.com>
2023-05-10 03:40:43 +0200

    h

--- run_tests.py
+++ run_tests.py
@@ -3,9 +3,10 @@ import subprocess
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 test_dir = os.path.join(root_dir, "tests")
+excluded_file = os.path.join(test_dir, "test_heroku.py")
 
 def run_tests():
-    result = subprocess.run(["pytest", test_dir, f"--rootdir={root_dir}"], capture_output=True, text=True)
+    result = subprocess.run(["pytest", test_dir,f"--ignore={excluded_file}",f"--rootdir={root_dir}"], capture_output=True, text=True)
     print(result.stdout)
     print(result.stderr)
 
