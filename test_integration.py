f04e9940d2069d2fb07e9700984b2723448e4c18
FUGERAY <hugo.fugeray@gmail.com>
2023-05-10 02:12:20 +0200

    h

--- test_integration.py
+++ test_integration.py
@@ -3,8 +3,27 @@ import httpx
 from api import api
 from fastapi import HTTPException
 
-def test_api_call_success():
+def test_api_call_success_customers():
     endpoint = "/customers"  # Utilisez un endpoint valide pour les tests
+    response_data = api(endpoint)
+
+    assert response_data is not None
+    assert isinstance(response_data, list)
+
+def test_api_call_success_order():
+
+    id = 8
+    endpoint = "/orders?id={id}"  # Utilisez un endpoint valide pour les tests
+    response_data = api(endpoint)
+
+    assert response_data is not None
+    assert isinstance(response_data, list)
+
+def test_api_call_success_order():
+
+    customer_id = 8
+    order_id = 8
+    endpoint = "/products?customer_id={customer_id}&order_id={order_id}"  # Utilisez un endpoint valide pour les tests
     response_data = api(endpoint)
 
     assert response_data is not None
