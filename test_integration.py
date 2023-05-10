85495e432b04d8d007374a60242bafd401607a39
FUGERAY <hugo.fugeray@gmail.com>
2023-05-10 02:07:24 +0200

    h

--- test_integration.py
+++ test_integration.py
@@ -1,10 +1,19 @@
 import pytest
 import httpx
 from api import api
+from fastapi import HTTPException
 
-def test_call_external_api():
+def test_api_call_success():
     endpoint = "/customers"  # Utilisez un endpoint valide pour les tests
-    response = api(endpoint)
+    response_data = api(endpoint)
 
-    assert response == 200
-    assert "data" in response
+    assert response_data is not None
+    assert isinstance(response_data, list)
+
+def test_api_call_failure():
+    invalid_endpoint = "/invalid/endpoint"  # Utilisez un endpoint non valide pour les tests
+
+    with pytest.raises(HTTPException) as exc_info:
+        api(invalid_endpoint)
+
+    assert exc_info.value.status_code == 500
