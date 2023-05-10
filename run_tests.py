e9c8dde7a3df192b1f0dc588b5b54a4a2d165955
FUGERAY <hugo.fugeray@gmail.com>
2023-05-10 03:41:59 +0200

    h

--- run_tests.py
+++ run_tests.py
@@ -3,7 +3,7 @@ import subprocess
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 test_dir = os.path.join(root_dir, "tests")
-excluded_file = os.path.join(test_dir, "test_heroku.py")
+excluded_file = os.path.join(test_dir, "run_test_heroku.py")
 
 def run_tests():
     result = subprocess.run(["pytest", test_dir,f"--ignore={excluded_file}",f"--rootdir={root_dir}"], capture_output=True, text=True)
