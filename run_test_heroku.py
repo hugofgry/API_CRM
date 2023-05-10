aff3738bef7adf41f5868a09d070b09d61822348
FUGERAY <hugo.fugeray@gmail.com>
2023-05-10 05:58:58 +0200

    h

--- run_test_heroku.py
+++ run_test_heroku.py
@@ -1,5 +1,5 @@
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+import smtplib
+from email.mime.text import MIMEText
 import os
 import subprocess
 
@@ -7,30 +7,48 @@ root_dir = os.path.abspath(os.path.dirname(__file__))
 test_dir = os.path.join(root_dir, "test_heroku")
 test_file = os.path.join(test_dir, "test_heroku.py")
 
+def send_email(subject, body, from_email, to_email, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = from_email
+    msg['To'] = to_email
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login(from_email, password)
+        server.sendmail(from_email, to_email, msg.as_string())
+        server.quit()
+        print("Email sent successfully")
+    except Exception as e:
+        print(f"Error while sending email: {e}")
 
 
-def send_email_on_failure():
-    message = Mail(
-        from_email="hugo.fugeray@gmail.com",
-        to_emails="payatonkawadeveloppeurs@gmail.com",
-        subject="Tests Failed on Heroku",
-        html_content="<strong>One or more tests failed on Heroku. Please check the logs for more details.</strong>"
-    )
-
-    sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
 
 def run_tests():
     result = subprocess.run(["pytest", test_file, f"--rootdir={root_dir}"], capture_output=True, text=True)
-    print(result.stdout)
-    print(result.stderr)
-    send_email_on_failure()
 
     if result.returncode != 0:
-        send_email_on_failure()
+        from_email = "payatonkawadeveloppeurs@gmail.com"
+        to_email = "hugo.fugeray@gmail.com"
+        password = "PayeTonKawa33000"
+        subject = "Test Failure"
+        body = f"Tests failed:\n\n{result.stdout}\n{result.stderr}"
+        send_email(subject, body, from_email, to_email, password)
+
+    else:
+        print("All tests passed")
+        from_email = "payatonkawadeveloppeurs@gmail.com"
+        to_email = "hugo.fugeray@gmail.com"
+        password = "PayeTonKawa33000"
+        subject = "Test success"
+        body = f"Tests failed:\n\n{result.stdout}\n{result.stderr}"
+        send_email(subject, body, from_email, to_email, password)
+    print(result.stdout)
+    print(result.stderr)
+
+
+    if result.returncode != 0:
+
 
 if __name__ == "__main__":
     run_tests()
